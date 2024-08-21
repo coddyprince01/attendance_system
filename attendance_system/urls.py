bcc7527896703757809e7a1c46d3a806ce2e9470
coddyprince01 <100879402+coddyprince01@users.noreply.github.com>
2024-08-21 08:08:48 +0000

    Updated urls.py with new configuration

--- attendance_system/urls.py
+++ attendance_system/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
+
 
 urlpatterns = [
     path('api/', include('attendance.urls')),
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponseRedirect('/api/')),  # Redirect root URL to /api/
 
 ]
